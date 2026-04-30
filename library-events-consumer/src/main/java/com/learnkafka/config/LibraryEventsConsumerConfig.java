9c6d635f9c181883f9efac22ba41a9bf569998e7
Marcos <m.alex.vi.silva@gmail.com>
2026-04-30 07:23:27 -0400

    feat: Create application.yml and LibraryEventsConsumerConfig

---
+++ library-events-consumer/src/main/java/com/learnkafka/config/LibraryEventsConsumerConfig.java
@@ -0,0 +1,9 @@
+package com.learnkafka.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.annotation.EnableKafka;
+
+@Configuration
+// @EnableKafka Needed for older versions of Spring Boot
+public class LibraryEventsConsumerConfig {
+}
